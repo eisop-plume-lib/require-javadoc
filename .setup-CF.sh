2ab3cf6b535be5cb145fa486b7fa18118621f80a
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-04-02 23:38:05 -0400

    Improve CI and downgrade gradle to 8.6

---
+++ .setup-CF.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo Entering "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in `pwd`
+
+# Fail the whole script if any command fails
+set -e
+
+export SHELLOPTS
+
+export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(readlink -f $(which javac))))}
+
+git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+
+export JSR308="${JSR308:-$(cd .. && pwd -P)}"
+export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)/../checker-framework}"
+
+## Build Checker Framework
+/tmp/plume-scripts/git-clone-related eisop checker-framework ${CHECKERFRAMEWORK}
+
+(cd $CHECKERFRAMEWORK && ./gradlew assemble)
+
+echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in `pwd`
