f56837c0a8f537a9300f842391147cd11c97ff2d
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-04-03 00:12:55 -0400

    Update setup script

--- .setup-CF.sh
+++ .setup-CF.sh
@@ -11,12 +11,11 @@ export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(readlink -f $(which javac)))
 
 git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
 
-export JSR308="${JSR308:-$(cd .. && pwd -P)}"
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)/../checker-framework}"
 
 ## Build Checker Framework
 /tmp/plume-scripts/git-clone-related eisop checker-framework ${CHECKERFRAMEWORK}
 
-(cd $CHECKERFRAMEWORK && ./gradlew assemble)
+(cd $CHECKERFRAMEWORK && ./gradlew assembleForJavac)
 
 echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in `pwd`
