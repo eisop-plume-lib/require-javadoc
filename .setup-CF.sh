861d873219cc28d40a4c77aa6f2494b86767fbba
Werner Dietl <wdietl@gmail.com>
2026-02-19 10:49:43 -0500

    Fix shell check errors

--- .setup-CF.sh
+++ .setup-CF.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo Entering "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in `pwd`
+echo Entering "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in "$(pwd)"
 
 # Fail the whole script if any command fails
 set -e
 
 export SHELLOPTS
 
-export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(readlink -f $(which javac))))}
+export JAVA_HOME="${JAVA_HOME:-$(dirname "$(dirname "$(readlink -f "$(which javac)")")")}"
 
 git -C /tmp clone --depth 1 -q https://github.com/eisop-plume-lib/git-scripts.git
 
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)/../checker-framework}"
 
 ## Build Checker Framework
-/tmp/git-scripts/git-clone-related eisop checker-framework ${CHECKERFRAMEWORK}
+/tmp/git-scripts/git-clone-related eisop checker-framework "${CHECKERFRAMEWORK}"
 
-(cd $CHECKERFRAMEWORK && ./gradlew assembleForJavac)
+(cd "$CHECKERFRAMEWORK" && ./gradlew assembleForJavac)
 
-echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in `pwd`
+echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in "$(pwd)"
